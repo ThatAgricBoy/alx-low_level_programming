a5f9104095dbb100c2b252963025a39526ddde75
Thatagricguy <samueljohn3999@gmail.com>
2022-12-13 04:16:07 -0800

    git add file and putchar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - This will print a sring followed by a new line
+  * Return: 0 will mean (success)
+  */
+
+int main(void)
+{
+	printf("/"_putchar"\n);
+	return (0);
+
+
+
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This readme file is for alx software engineering program describing functions and nested loops
