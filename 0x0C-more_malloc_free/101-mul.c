7014000c8c4cab9021f2a9814d37495f3a90e811
Thatagricguy <samueljohn3999@gmail.com>
2023-01-12 11:25:53 -0800

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,48 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
+#include <stdio.h>
 
-void usage() {
-    fprintf(stderr, "Usage: mul num1 num2\n");
-    exit(98);
+#define ERR_MSG "Error"
+
+int _isdigit(char *s);
+
+/**
+ * main - takes two numbers as arguments and outputs the product.
+ * @argc: the number of arguments including name of program.
+ * @argv: an array of arguments
+ *
+ * Return: 0 if successful.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 3)
+	{
+		for (i = 0; ERR_MSG[i] != '\0'; i++)
+			_putchar(ERR_MSG[i]);
+		_putchar(10);
+		exit(98);
+	}
+	if (_isdigit(argv[1]) == 0 && _isdigit(argv[2]) == 0)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
 
-void error() {
-    fprintf(stderr, "Error\n");
-    exit(98);
+/**
+ * _isdigit - checks if string is a number or not
+ * @s: the string to be checked
+ *
+ * Return: 1 if true, else 0.
+ */
+
+int _isdigit(char *s)
+{
+	while (*s)
+	{
+		if (*s < 48 || *s > 57)
+			return (1);
+		s++;
+	}
+	return (0);
 }
-
-int check_digit(char *str) {
-    for (int i = 0; str[i]; i++) {
-        if (!isdigit(str[i])) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        usage();
-    }
-
-    if (!check_digit(argv[1]) || !check_digit(argv[2])) {
-        error();
-    }
-
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    printf("%d\n", num1 * num2);
-    return 0;
-}
-
