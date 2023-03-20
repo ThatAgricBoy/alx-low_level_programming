b8b283fd7bea1c86f9cf6e99b0cb130d837d2fb5
ThatAgricBoy <samueljohn3999@gmail.com>
2023-03-20 07:55:08 -0700

    'correctionss

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,64 +1,72 @@
-#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-*op_add - Returns the sum of two numbers
-*@a:the first number
-*@b:the second number
-*
-*Return:The sum of a and b
-*/
-
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: add.
+ */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
-
 /**
-*op_sub - Returns the substraction of two numbers
-*@a:The first number
-*@b:The second number
-*
-*Return: The Difference of a and b
-*/
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference.
+ */
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 
 /**
-*op_mul - Returns the multiplication of two numbers
-*@a:The first number
-*@b: The second number
-*
-*Return: The multiplication of a and b
-*/
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
+ */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 
-
 /**
-*op_div - Returns the division of two numbers.
-*@a: The first number.
-*@b: The second number.
-*
-*Return: The quotient of a and b.
-*/
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
+ */
 int op_div(int a, int b)
 {
-return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
 /**
-*op_mod - Returns the remainder of the division of two numbers.
-*@a: The first number.
-*@b: The second number.
-*
-*Return: The remainder of the division of a by b.
-*/
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
+ */
 int op_mod(int a, int b)
 {
-return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
