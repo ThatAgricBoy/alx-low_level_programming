2c9ba7fbf7109a190327cc266718a0c6aa9130ec
ThatAgricBoy <samueljohn3999@gmail.com>
2023-03-20 08:24:22 -0700

    correction

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NicolasTfile/alx-low_level_programming/blob/master/0x18-dynamic_libraries/giga.so
-export LD_PRELOAD=/tmp/giga.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
